15dee80f86104e807089a21a1edb4b807f3d511e
Kris Stern <krisstern@outlook.com>
2023-09-26 04:37:20 +0800

    Add favicon and background images

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,12 +6,21 @@ module.exports = {
     title: `Jenkins Contributor Spotlight`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-google-gtag", "gatsby-plugin-image", "gatsby-plugin-sitemap", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-sitemap", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
-      "path": "./src/images/"
+      "path": "./src/images/",
     },
     __key: "images"
+  },{
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      name: `Jenkins Contributor Spotlight`,
+      short_name: `Contributor Spotlight`,
+      start_url: `/`,
+      display: `standalone`,
+      icon: `src/images/jenkins.png`,
+    },
   }]
 };
