34ae8e5b13f2187ad804206c143031c96501f51d
Kris Stern <krisstern@outlook.com>
2023-12-13 21:31:38 +0800

    feat: add eslint and prettier and add og metadata

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+    env: {
+        browser: true,
+        node: true,
+        es2021: true,
+    },
+    extends: ['eslint:recommended', 'google', 'prettier'],
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: ['.eslintrc.{js,cjs}'],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+            modules: true,
+            experimentalObjectRestSpread: true,
+        },
+    },
+    plugins: ['react'],
+    rules: {
+        'react/jsx-curly-brace-presence': [
+            'error',
+            { props: 'never', children: 'never' },
+        ],
+        'react/jsx-key': 'error',
+        semi: 'error',
+        'prefer-const': 'error',
+        'no-unused-vars': 'off',
+        'require-jsdoc': 'off',
+    },
+};
