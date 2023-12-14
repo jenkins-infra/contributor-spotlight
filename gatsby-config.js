8f454e2ae2aca7c1be11d4d524ca5c2971dc1534
Kris Stern <krisstern@outlook.com>
2023-12-14 08:28:11 +0800

    feat: add `eslint` and `prettier` and add opengraph metadata (#57)
    * feat: add eslint and prettier and add og metadata
    
    * feat: remove unused plugin
    
    * feat: add dayjs as dependency and additional og metadata
    
    * feat: update homepage og metadata
    
    * fix: debug
    
    * fix: patch linting and formatting issues
    
    * feat: try another og image for homepage and a longer description
    
    * feat: try some other dimensions for contributor og
    
    * feat: add og:title to og metadata to avoid confusion

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,46 +2,48 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  siteMetadata: {
-    title: `Jenkins Contributor Spotlight`,
-    siteUrl: `https://www.jenkins.io`,
-    description: `Jenkins Contributor Spotlight is where we celebrate the contributions of Jenkins community members`,
-  },
-  plugins: [
-    "gatsby-plugin-image",
-    "gatsby-plugin-sitemap",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-react-helmet", {
-    resolve: `gatsby-source-filesystem`,
-    options: {
-      name: `pages`,
-      path: `${__dirname}/src`,
+    siteMetadata: {
+        title: `Jenkins Contributor Spotlight`,
+        siteUrl: `https://contributors.jenkins.io`,
+        description: `Jenkins Contributor Spotlight is where we celebrate the contributions of Jenkins community members`,
     },
-  }, {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      name: `Jenkins Contributor Spotlight`,
-      short_name: `Contributor Spotlight`,
-      start_url: `/`,
-      display: `standalone`,
-      icon: `${__dirname}/src/images/jenkins.png`,
-    },
-  }, `gatsby-transformer-asciidoc`,
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `${__dirname}/src/utils/typography`,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-asciidoc`,
-      options: {
-        attributes: {
-          imagesdir: `${__dirname}/static@`,
+    plugins: [
+        'gatsby-plugin-image',
+        'gatsby-plugin-sitemap',
+        'gatsby-plugin-sharp',
+        'gatsby-transformer-sharp',
+        'gatsby-plugin-react-helmet',
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `pages`,
+                path: `${__dirname}/src`,
+            },
         },
-      },
-    },
-  ]
-};
-
+        {
+            resolve: 'gatsby-plugin-manifest',
+            options: {
+                name: `Jenkins Contributor Spotlight`,
+                short_name: `Contributor Spotlight`,
+                start_url: `/`,
+                display: `standalone`,
+                icon: `${__dirname}/src/images/jenkins.png`,
+            },
+        },
+        `gatsby-transformer-asciidoc`,
+        {
+            resolve: `gatsby-plugin-typography`,
+            options: {
+                pathToConfigModule: `${__dirname}/src/utils/typography`,
+            },
+        },
+        {
+            resolve: `gatsby-transformer-asciidoc`,
+            options: {
+                attributes: {
+                    imagesdir: `${__dirname}/static@`,
+                },
+            },
+        },
+    ],
+}
