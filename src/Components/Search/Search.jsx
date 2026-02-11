c171bf7733d48b0e5e0c337a28a5c8a8f8ea78bf
Vaibhaviitian <aryanvaibhav74@gmail.com>
2026-02-12 02:22:37 +0530

    creating new_branch for search functionality

---
+++ src/Components/Search/Search.jsx
@@ -0,0 +1,91 @@
+import React, { useEffect, useMemo, useRef, useState } from 'react';
+import { motion } from 'framer-motion';
+import { Search as SearchIcon } from 'lucide-react';
+import './search.css';
+import Fuse from 'fuse.js';
+import SearchResults from './SearchResults.jsx';
+function Search({ contributors, darkmode }) {
+    const [searchQuery, setSearchQuery] = useState('');
+    const [isFocused, setIsFocused] = useState(false);
+    const [results, setResults] = useState([]);
+    const searchInputRef = useRef(null);
+    const contributorsArray = useMemo(() => {
+        return Object.values(contributors).map((c) => ({
+            id: c?.node?.id,
+            name: c?.node?.document?.title || c?.document?.title || '',
+            location: c?.node?.pageAttributes?.location || '',
+            github: c?.node?.pageAttributes?.github || '',
+            linkedin: c?.node?.pageAttributes?.linkedin || '',
+            twitter: c?.node?.pageAttributes?.twitter || '',
+            firstcommit: c?.node?.pageAttributes?.firstcommit || '',
+            slug: c?.node?.fields?.slug || '/',
+            image: c?.node?.pageAttributes?.image || '',
+        }));
+    }, [contributors]);
+    const idx = useMemo(() => {
+        const keys = ['name', 'location'];
+        return new Fuse(contributorsArray, {
+            keys,
+            threshold: 0.6,
+            includeScore: true,
+            ignoreLocation: true,
+            findAllMatches: true,
+        });
+    }, [contributorsArray]);
+
+    useEffect(() => {
+        if (!searchQuery.trim()) {
+            setResults([]);
+            return;
+        }
+        const results = idx.search(searchQuery);
+        setResults(results);
+    }, [searchQuery, idx]);
+
+    useEffect(() => {
+        const handleKeyDown = (e) => {
+            if ((e.ctrlKey || e.metaKey) && e.key.toLowerCase() === 'k') {
+                e.preventDefault();
+                searchInputRef.current?.focus();
+            }
+        };
+        document.addEventListener('keydown', handleKeyDown);
+        return () => {
+            document.removeEventListener('keydown', handleKeyDown);
+        };
+    }, []);
+    return (
+        <div className={`search ${darkmode ? 'dark' : 'light'}`}>
+            <motion.div className='search-container-wrapper'>
+                <motion.div
+                    className={`search-input-container ${isFocused ? 'focused' : ''}`}
+                    whileHover={{ scale: 1.02 }}
+                    transition={{
+                        type: 'tween',
+                        duration: 0.15,
+                        ease: 'easeOut',
+                    }}
+                >
+                    <SearchIcon size={20} className='search-icon' />
+                    <input
+                        ref={searchInputRef}
+                        type='text'
+                        value={searchQuery}
+                        onChange={(e) => setSearchQuery(e.target.value)}
+                        onFocus={() => setIsFocused(true)}
+                        onBlur={() => setIsFocused(false)}
+                        placeholder='[Ctrl + k] Search contributors...'
+                        className='search-input'
+                    />
+                </motion.div>
+                <div>
+                    {searchQuery && (
+                        <SearchResults results={results} darkmode={darkmode} />
+                    )}
+                </div>
+            </motion.div>
+        </div>
+    );
+}
+
+export default Search;
