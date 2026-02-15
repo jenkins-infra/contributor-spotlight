9a6b677bbc93063816a85156d19a035822478c61
Vaibhaviitian <aryanvaibhav74@gmail.com>
2026-02-16 01:37:21 +0530

    adding all files

--- src/Components/Contributor/ContributorCard.jsx
+++ src/Components/Contributor/ContributorCard.jsx
@@ -7,7 +7,7 @@ import {
     Linkedin,
     CircleUser,
 } from 'lucide-react';
-import XIcon from './XIcon';
+import XIcon from '../XIcon';
 
 const ContributorCard = ({ contributor }) => {
     const { pageAttributes } = contributor?.node;
