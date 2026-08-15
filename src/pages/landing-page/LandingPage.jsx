3b2fe80ba82d33ba73aaee73049aa02dfdbc5edb
Vatsal-Verma <lastav1234@gmail.com>
2026-08-15 16:16:30 +0530

    feat: add hero section for contributor spolight

--- src/pages/landing-page/LandingPage.jsx
+++ src/pages/landing-page/LandingPage.jsx
@@ -1,5 +1,6 @@
 import { slugs, loadContributor } from '../../utils/contributorLoad';
 import ContributorCard from '../../components/contributor-card/ContributorCard';
+import HeroSection from '../../components/hero-section/HeroSection';
 import './LandingPage.css';
 
 function LandingPage() {
@@ -10,6 +11,7 @@ function LandingPage() {
 
     return (
         <main className='landing-page'>
+            <HeroSection />
             <section className='contributors-section'>
                 <div className='contributors-grid'>
                     {contributors.map((contributor) => (
