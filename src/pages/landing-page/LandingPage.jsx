cf26e4f401d84d122ad205c4587df26dc3240424
Vatsal-Verma <lastav1234@gmail.com>
2026-08-19 13:28:07 +0530

    Revamp: Add contributor spotlight component

--- src/pages/landing-page/LandingPage.jsx
+++ src/pages/landing-page/LandingPage.jsx
@@ -1,7 +1,10 @@
 import { slugs, loadContributor } from '../../utils/contributorLoad';
 import ContributorCard from '../../components/contributor-card/ContributorCard';
 import HeroSection from '../../components/hero-section/HeroSection';
+import SpotLight from '../../components/spotlight/ContributorSpotlight';
 import './LandingPage.css';
+
+const SPOTLIGHT_SLUG = 'allan-burdajewicz';
 
 function LandingPage() {
     const contributors = slugs.map((slug) => ({
@@ -9,9 +12,18 @@ function LandingPage() {
         ...loadContributor(slug),
     }));
 
+    const featuredContributor = contributors.find(
+        (contributor) => contributor.slug === SPOTLIGHT_SLUG
+    );
+
     return (
         <main className='landing-page'>
             <HeroSection />
+
+            {featuredContributor && (
+                <SpotLight contributor={featuredContributor} />
+            )}
+
             <section className='contributors-section'>
                 <div className='contributors-grid'>
                     {contributors.map((contributor) => (
